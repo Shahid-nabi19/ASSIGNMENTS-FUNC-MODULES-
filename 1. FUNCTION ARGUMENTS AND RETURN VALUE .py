ac185a0eb8800bc8b80624014bedbc7d3b73275e
shahid Nabi <shahidnabi850@gmail.com>
2024-12-21 18:40:00 +0530

    Update and rename 1. BASIC ARITHMATIC FUNCTION .py to 1. FUNCTION ARGUMENTS AND RETURN VALUE .py

--- 1. FUNCTION ARGUMENTS AND RETURN VALUE .py
+++ 1. FUNCTION ARGUMENTS AND RETURN VALUE .py
@@ -1,3 +1,5 @@
+# BASIC ARITHMETIC FUNCTION.
+
 def calculate(a,o,b):
     if o=="+":
         return a+b
@@ -9,3 +11,12 @@ def calculate(a,o,b):
         return a/b
 result=calculate(2,"+",2)
 print(result)
+
+
+# DEFAULT AND KEYWORD ARGUMENTS. 
+
+def greet(a, b="HI "):
+    return b+ a
+result=greet("Shahid")
+result2= greet("Shahid", " ASSLAM ALIKUM ")
+print(result,"\n",result2)
